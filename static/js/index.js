583f13a5fbc2064339b60da2da50244d63a8370a
yosh <yoshtalwar@gmail.com>
2016-02-22 15:37:07 +0530

    Parallaxes the hero h1

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,20 @@
+'use strict'
+
+import scrolltop from 'scrolltop'
+
 const app = document.getElementById('app')
+const heroHeader = document.querySelector('.hero h1')
 
 window.addEventListener('load', () => {
   app.classList.add('visible')
+})
+
+window.addEventListener('scroll', e => {
+  const scrollOffset = scrolltop()
+
+  if (scrollOffset < window.innerHeight) {
+    const ratio = parseFloat((scrollOffset / window.innerHeight).toFixed(3))
+    heroHeader.style.opacity = 1 - ratio
+    heroHeader.style.transform = `translateY(${(1-scrollOffset)/6}px)`
+  }
 })
